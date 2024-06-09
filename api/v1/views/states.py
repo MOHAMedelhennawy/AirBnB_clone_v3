1a95176ae050f076d2195ad19630534de8f3d789
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-06-09 18:25:48 +0300

    Add documentation

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""new view for State objects that handles all default RESTFul API actions
+"""
 from models.state import State
 from api.v1.views import app_views
 from models import storage
