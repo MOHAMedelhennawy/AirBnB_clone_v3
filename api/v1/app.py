31c76e51f6a92a16175d3bad102e9ecfbcf2fdd8
Jihan Ahmed <jihanahmed41@gmail.com>
2024-06-10 12:28:04 -0700

    Task 12 HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,10 +6,12 @@ from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
+CORS(app, origins=["*"])
 
 
 @app.teardown_appcontext
