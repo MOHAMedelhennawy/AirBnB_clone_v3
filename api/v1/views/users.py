b752eb7937eaf044f73f4a97218d20f205e2ac51
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-06-10 12:19:07 +0300

    Add users RESTfull api

---
+++ api/v1/views/users.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""new view for State objects that handles all default RESTFul API actions
+"""
+from models.user import User
+import json
+from api.v1.views import app_views
+from models import storage
+from flask import jsonify, abort, request
+
+
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
+def getUsers():
+    """Retrieves all users"""
+    users = [user.to_dict() for user in storage.all(User).values()]
+    return jsonify(users)
+
+@app_views.route('/users/<user_id>', methods=['GET'])
+def getUserWithID(user_id):
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    return jsonify(user.to_dict())
+
+@app_views.route('/users/<user_id>', methods=['DELETE'])
+def deleteUserWithID(user_id):
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    storage.delete(user)
+    storage.save()
+    return jsonify({}), 200
+
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
+def createUser():
+    data = request.get_json()
+    if not isinstance(data, dict):
+        abort(400, {'error': 'Not a JSON'})
+    if 'email' not in data:
+        abort(400, 'Missing email')
+    if 'password' not in data:
+        abort(400, 'Missing password')
+    new_user = User(**data)
+    storage.new(new_user)
+    new_user.save()
+    return jsonify(new_user.to_dict()), 201
+
+@app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
+def updateUser(user_id):
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+    data = request.get_json()
+    if not isinstance(data, dict) or not data:
+        abort(400, {'error': 'Not a JSON'})
+    for key, value in data.items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(user, key, value)
+    storage.save()
+    return jsonify(user.to_dict()), 200
