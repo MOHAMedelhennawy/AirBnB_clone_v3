98c9ff0e815b2a560b06f4c9e95c2fa212ccd202
Jihan Ahmed <jihanahmed41@gmail.com>
2024-06-09 20:44:49 -0700

    not ajson post

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -43,18 +43,15 @@ def deleteState(state_id):
 @app_views.route('/states/', methods=['POST'], strict_slashes=False)
 def createState():
     """Creates a new state"""
-    try:
-        data = request.get_json()
-        if not isinstance(data, dict):
-            abort(400, {'error': 'Not a JSON'})
-        if 'name' not in data:
-            abort(400, {'error': 'Missing name'})
-        new_state = State(**data)
-        storage.new(new_state)
-        new_state.save()
-        return jsonify(new_state.to_dict()), 201
-    except BadRequest:
+    data = request.get_json()
+    if not isinstance(data, dict):
         abort(400, {'error': 'Not a JSON'})
+    if 'name' not in data:
+        abort(400, {'error': 'Missing name'})
+    new_state = State(**data)
+    storage.new(new_state)
+    new_state.save()
+    return jsonify(new_state.to_dict()), 201
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
