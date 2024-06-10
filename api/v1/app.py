95b2b7f5fff25483d598a7849119b9bc5676a3ac
Jihan Ahmed <jihanahmed41@gmail.com>
2024-06-10 12:33:05 -0700

    Task 12 HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,6 +13,7 @@ app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
+
 @app.teardown_appcontext
 def close(e):
     """ close the storage """
